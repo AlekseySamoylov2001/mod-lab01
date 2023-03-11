c1a51738a264e65821cd1b7d5ebc666699c698ca
Samoylov Aleksey <blinchik232@inbox.ru>
2023-03-11 21:01:28 +0300

    Update fun.cpp
    task 2

--- src/fun.cpp
+++ src/fun.cpp
@@ -33,8 +33,41 @@ unsigned int faStr1(const char* str) {
 	return nbWords;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+	bool isSpace = true;
+	bool isIncorrect = true;
+	int nbWords = 0;
+
+	for (int itStr = 0; str[itStr] != '\0';) {
+		if (str[itStr] < 'A' || str[itStr] > 'Z') {
+			while (str[itStr] != ' ') {
+				if (str[itStr] == '\0') {
+					break;
+				}
+				++itStr;
+			}
+
+			continue;
+		}
+
+		if (str[itStr] != ' ') {
+			isSpace = false;
+			if (str[itStr] < 'a' || str[itStr] > 'z') {
+				isIncorrect = true;
+			}
+		}
+		else {
+			if (!isSpace && !isIncorrect) {
+				++nbWords;
+				isSpace = true;
+				isSpace = false;
+			}
+		}
+
+		++itStr;
+	}
+
+	return nbWords;
 }
 
 unsigned int faStr3(const char *str) {
