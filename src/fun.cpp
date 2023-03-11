b8487a19372fe653962de52fba977acfb0d410b4
Samoylov Aleksey <blinchik232@inbox.ru>
2023-03-11 22:13:49 +0300

    edited style and fix task 2

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,86 +2,86 @@
 #include "fun.h"
 
 unsigned int faStr1(const char* str) {
-	bool isCorrect = true;
-	bool isSpace = true;
-	int nbWords = 0;
+    bool isCorrect = true;
+    bool isSpace = true;
+    int nbWords = 0;
 
-	for (int itStr = 0; str[itStr] != '\0'; ++itStr) {
-		if (str[itStr] >= '0' && str[itStr] <= '9') {
-			isCorrect = false;
-		}
-		if (str[itStr] != ' ') {
-			isSpace = false;
-		} else {
-			if (!isSpace && isCorrect) {
-				++nbWords;
-			}
-			isSpace = true;
-			isCorrect = true;
-		}
-	}
+    for (int itStr = 0; str[itStr] != '\0'; ++itStr) {
+        if (str[itStr] >= '0' && str[itStr] <= '9') {
+            isCorrect = false;
+        }
+        if (str[itStr] != ' ') {
+            isSpace = false;
+        } else {
+        	if (!isSpace && isCorrect) {
+            ++nbWords;
+            }
+            isSpace = true;
+            isCorrect = true;
+        }
+    }
 
-	return nbWords;
+    return nbWords;
 }
 
 unsigned int faStr2(const char* str) {
-	bool isIncorrect = false;
-	int nbWords = 0;
+    bool isSpace = true;
+    bool isIncorrect = false;
+    int nbWords = 0;
 
-	for (int itStr = 0; str[itStr] != '\0';) {
-		if (str[itStr] < 'A' || str[itStr] > 'Z') {
-			while (str[itStr] != ' ') {
-				if (str[itStr] == '\0') {
-					break;
-				}
-				++itStr;
-			}
+    for (int itStr = 0; str[itStr] != '\0';) {
+        if (str[itStr] < 'A' || str[itStr] > 'Z') {
+            while (str[itStr] != ' ') {
+                if (str[itStr] == '\0') {
+                    break;
+                }
+                ++itStr;
+            }
 
-			continue;
-		}
+            continue;
+        }
 
-		while (str[itStr] != ' ') {
-			if (str[itStr] == '\0') {
-				break;
-			}
-			if (str[itStr] < 'a' || str[itStr] > 'z') {
-				isIncorrect = true;
-			}
+        if (str[itStr] != ' ') {
+            isSpace = false;
+            if (str[itStr] < 'a' || str[itStr] > 'z') {
+                isIncorrect = true;
+            }
+        }
+        else {
+            if (!isSpace && !isIncorrect) {
+                ++nbWords;
+            }
+            isSpace = true;
+            isIncorrect = false;;
+        }
 
-			++itStr;
-		}
+        ++itStr;
+    }
 
-		if (!isIncorrect) {
-			++nbWords;
-		}
-
-		isIncorrect = false;
-	}
-
-	return nbWords;
+    return nbWords;
 }
 
 unsigned int faStr3(const char* str) {
-	int nbWords = 0;
-	double lenght = 0;
-
-	for (int itStr = 0; str[itStr] != '\0';) {
-		if (str[itStr] != ' ') {
-			++nbWords;
-			while (str[itStr] != ' ') {
-				if (str[itStr] == '\0') {
-					break;
-				}
-				++lenght;
-				++itStr;
-			}
-		}
-
-		++itStr;
-	}
-
-	if (nbWords > 0) {
-		lenght /= nbWords;
-	}
-	return static_cast<int>(lenght + 0.5);
+    int nbWords = 0;
+    double lenght = 0;
+    
+    for (int itStr = 0; str[itStr] != '\0';) {
+        if (str[itStr] != ' ') {
+            ++nbWords;
+            while (str[itStr] != ' ') {
+                if (str[itStr] == '\0') {
+                    break;
+                }
+                ++lenght;
+                ++itStr;
+            }
+        }
+        
+        ++itStr;
+    }
+    
+    if (nbWords > 0) {
+        lenght /= nbWords;
+    }
+    return static_cast<int>(lenght + 0.5);
 }
